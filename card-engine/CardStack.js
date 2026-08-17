bd4c127bdda0577af5656a420adce28d5815c4ab
Shornana <russellemshanahan@yahoo.com>
2026-08-17 21:29:21 +0100

    Created general structure of CardStack.

--- card-engine/CardStack.js
+++ card-engine/CardStack.js
@@ -1,5 +1,9 @@
 export class CardStack{
-  constructor(stack){
+  constructor(stack, minimum, maximum){
     this.stack = stack;
+    this.minimumNumberOfCards = minumum;
+    this.maximumNumberOfCards = maximum;
   }
+
+  take(){}
 }
