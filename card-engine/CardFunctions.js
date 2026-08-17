183f11d6ab71146c6603e595c46ab69287b00df7
Shornana <russellemshanahan@yahoo.com>
2026-08-17 22:13:20 +0100

    Added comments to CardFunctions to describe implementation.

--- card-engine/CardFunctions.js
+++ card-engine/CardFunctions.js
@@ -1,5 +1,15 @@
 export class CardFunctions {
-  move(sourceCard, targetStack){}
-  swap(sourceCard, targetCard){}
-  deal(player, stack){}
+  move(sourceCard, targetStack){
+    //Need to transfer ownership of the card.
+    //Therefore we need to remove the sourceCard from the owner's stack.
+    //Once we have done that, we need to append it to the target stack.
+    //Perhaps the removal implementation can be handled by the mover.
+  }
+  swap(sourceCard, targetCard){
+    //Need to transfer ownership of each card to one another.
+    //We can use temporary objects to transfer the cards.
+  }
+  deal(player, stack){
+    //We simply need to move the top card from stack to the player.
+  }
 }
